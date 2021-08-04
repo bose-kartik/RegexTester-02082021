79fa87a314756009456900dc44b41f7228dc392a
kakarr0tt0 <kartik.bose@hotmail.com>
2021-08-05 00:56:51 +0530

    Interfaced from File extracted as Dictionary
    [+]Interfaces are first extracted as Interface Blocks\n[+]Then those Interface Blocks run through Regex.\n[+]Regex Matches are added to Dictionary.\n[+]Dictionaries are combined to make a List of Interfaces as requested in the Task

--- _regex_test.py
+++ _regex_test.py
@@ -1,3 +1,5 @@
+import re
+
 def interface_section():
     global interface_tracker
     line_number = 0 ### Variable to track the on which line number, the function is working.
@@ -10,8 +12,8 @@ def interface_section():
         line_number = line_number + 1
 
     start_ptr_count = len(interface_start_pointer)
-    for n in range(1,start_ptr_count): ### End Pointer for N-1th interface blocks,
-        interface_end_pointer.append(int(interface_start_pointer[n])-2)
+    for n in range(1,start_ptr_count): ### End Pointers for N-1th interface blocks.
+        interface_end_pointer.append(int(interface_start_pointer[n])-1)
 
     
     line_count = len(line)
@@ -20,12 +22,45 @@ def interface_section():
         if current_line == '\n' :
             interface_end_pointer.append(k)
             break
-    
-    
 
+def interface_block(StartPtr,EndPtr):
+    interface_list_tracker = 0
+    interface_block_dict = {
+        'name':'',
+        'ip' :'',
+        'mask':'',
+        'description':''
+    }
+    interface_block_dict.clear
+    
+    for current_line in range(StartPtr,EndPtr):
+
+        current_line_str = str(line[current_line])
+        
+        result = re.search(r'interface\s(\S+)', current_line_str , flags=0)
+        if result:
+            interface_block_dict.update(name= result.group(1))
+
+        result = re.search(r'description\s(\S+)', current_line_str , flags=0)
+        if result:
+            interface_block_dict.update(description= result.group(1))
+
+        result = re.search(r'ip\saddress\s(\S+)', current_line_str , flags=0)
+        if result:
+            spilt_result = result.group(1)
+            ip_result = re.search(r'(\S+)\/', current_line_str , flags=0)
+            if ip_result:
+                interface_block_dict.update(ip= ip_result.group(1))
+            mask_result = re.search(r'\/(\S+)', current_line_str , flags=0)
+            if mask_result:
+                interface_block_dict.update(mask= mask_result.group(1))
+    
+    interfaces.append(interface_block_dict)
+            
 interface_tracker = 0 ### Variable to track the number of interface present in File.
 interface_start_pointer = [] ### List to track the Start Line Number of Interface Blocks
 interface_end_pointer = [] ### List to track the End Line Number of Interface Blocks
+interfaces = []
 
 with open('sample.txt', 'r') as getter: ### Reading (and closing) the File : sample.txt
     line = getter.readlines() #### This makes the data as type : list
@@ -34,11 +69,11 @@ with open('sample.txt', 'r') as getter: ### Reading (and closing) the File : sam
     interface_end_pointer.clear()
     interface_section() ### Saves Start & End Pointers to all the interface blocks
 
+    ### Create Interface Blocks based on Start and End Ptr
+    for k in range(len(interface_start_pointer)):
+        interface_block(interface_start_pointer[k],interface_end_pointer[k])
 
-        
-#print('Total Interfaces in File : ' + str(interface_tracker))
-print(interface_start_pointer)
-print(interface_end_pointer)
+print(interfaces)
 
         
 
